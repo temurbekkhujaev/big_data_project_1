a6c14ff731deb232a35f1c88f49f6d955aaad3b6
temurbekkhujaev <temurbekkhujaev@gmail.com>
2020-09-24 22:54:15 +0300

    Indexer

--- src/Indexer.java
+++ src/Indexer.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Indexer {
+import indexing_engine.CorpusParser;
+import indexing_engine.DocumentCounter;
+import indexing_engine.VectorGenerator;
+import indexing_engine.WordEnumerator;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+public class Indexer {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("Usage:\n$hadoop jar <jar_name>.jar Indexer " +
+                    "<path to docs on HDFS> <path to output directory on HDFS>" +
+                    "\nExample:\n$hadoop jar ibdhmw.jar Indexer " +
+                    "/docs /indexed_docs");
+            System.out.println("your args " + args[0] + " " + args[1]);
+
+            return;
+        }
+        // input corpus files directory
+        Path corpusPath = new Path(args[0]);
+        // directory for job outputs to save in
+        Path outputDir = new Path(args[1]);
+        // Remove output dir, if exists
+        FileSystem fs = FileSystem.get(new Configuration());
+        if (fs.exists(outputDir)) {
+            fs.delete(outputDir, true);
+        }
+        fs.close();
+        // Run modules
+        Path parsedCorpusPath = CorpusParser.run(corpusPath, outputDir, CorpusParser.PARSE_TEXT);
+        CorpusParser.run(corpusPath, outputDir, CorpusParser.PARSE_TITLE_URL);
+        Path docCountPath = DocumentCounter.run(parsedCorpusPath, outputDir);
+        Path wordEnumPath = WordEnumerator.run(parsedCorpusPath, outputDir);
+        VectorGenerator.run(parsedCorpusPath, wordEnumPath, docCountPath, outputDir);
+    }
 }
